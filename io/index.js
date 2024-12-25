60078ea3cb35ad92cdc7d522f6de6012a006e4a8
ToptalDev <akira.murayama.dev@gmail.com>
2024-12-26 04:28:20 +0900

    Update index.js

--- io/index.js
+++ io/index.js
@@ -1,7 +1,7 @@
 module.exports = function () {
   const server = require('http').createServer(this.nuxt.renderer.app)
   const io = require('socket.io')(server)
-
+ 
   // overwrite nuxt.listen()
   this.nuxt.listen = (port, host) => new Promise((resolve) => server.listen(port || 3000, host || 'localhost', resolve))
   // close this server on 'close' event
